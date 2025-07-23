c790deef1c8d7e22a3aaa9404bc82b2acc8c8e1f
Adenike Awofeso <adenike.awofeso@ng.ab-inbev.com>
2025-07-23 17:33:58 +0100

    fix f string formatting

--- Smart Gardening/actuators/pump.py
+++ Smart Gardening/actuators/pump.py
@@ -1,5 +1,5 @@
 def activate_pump(zone_id: str):
-    print("f[PUMP] Zone {zone_id}: Water pump activated")
+    print(f"[PUMP] Zone {zone_id}: Water pump activated")
 
 def deactivate_pump(zone_id: str):
-    print("f[PUMP] Zone {zone_id}: Water pump deactivated")
+    print(f"[PUMP] Zone {zone_id}: Water pump deactivated")
