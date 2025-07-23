e975a91fb712647f0770f5974102ec68d4d992a9
Adenike Awofeso <adenike.awofeso@ng.ab-inbev.com>
2025-07-23 17:29:36 +0100

    replace similator fnctions with single Class

--- Smart Gardening/sensors/moisture_sensor.py
+++
@@ -1,4 +0,0 @@
-import random
-
-def simulate_moisture() -> float:
-    return round(random.uniform(20,80),2)
--- Smart Gardening/sensors/ph_sensor.py
+++
@@ -1,4 +0,0 @@
-import random
-
-def simulate_ph():
-    return round(random.uniform(4.5, 8.5), 1)
